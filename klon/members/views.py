4140ade43bab54f3e40a0493f47ba72f3debbf83
Exhil95 <d.kuczka@gmail.com>
2025-03-01 10:54:55 +0100

    zrobienie "kanwy" strony loginu

--- klon/members/views.py
+++ klon/members/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def login_user(request):
+    return render(request, 'authenticate/login.html', {})
+    
+
