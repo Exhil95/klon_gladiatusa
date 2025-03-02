cebb2655b17ee65957b252f1720235f73fcc7d94
dutis <m.m.rduch@gmail.com>
2025-03-02 11:30:51 +0100

    dodanie authentication

--- klon/members/views.py
+++ klon/members/views.py
@@ -5,8 +5,20 @@ from django.contrib import messages
 
 
 def login_user(request):
-
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('login_user')
+        else:
+            # Return an 'invalid login' error message.
+            return redirect('login')
+            pass
     
-    return render(request, 'authenticate/login.html', {})
+    
+    else:
+        return render(request, 'authenticate/login.html', {})
     
 
