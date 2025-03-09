301b0b9402d529710d4a4c3c8a852f22c25b171c
Exhil95 <d.kuczka@gmail.com>
2025-03-09 14:02:49 +0100

    dodanie atrybutów do db

--- klon/user_profile/models.py
+++ klon/user_profile/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    strength = models.IntegerField(default=1)
+    dexterity = models.IntegerField(default=1)
+    constitution = models.IntegerField(default=1)
+    intelligence = models.IntegerField(default=1)
     
     def __str__(self):
         return self.user.username
