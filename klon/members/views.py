b1f6e5d826eacf9df39f84bb1b3fad1119a51d7a
dutis <m.m.rduch@gmail.com>
2025-03-10 17:14:25 +0100

    dodano funkcje rejestracji

--- klon/members/views.py
+++ klon/members/views.py
@@ -26,3 +26,19 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("Wylogowano"))
     return redirect('login_user')
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Zarejestrowano"))
+            return redirect('profil')
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'authenticate/register.html', {'form': form})
