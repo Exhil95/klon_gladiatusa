a8191800800f1705c9102459b0402d47e727d966
Exhil95 <d.kuczka@gmail.com>
2025-03-10 13:37:14 +0100

    update modelu graczxa o doświadczenie i o rozdawanie pkt statystyk

--- klon/user_profile/models.py
+++ klon/user_profile/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import math
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,6 +8,26 @@ class UserProfile(models.Model):
     dexterity = models.IntegerField(default=1)
     constitution = models.IntegerField(default=1)
     intelligence = models.IntegerField(default=1)
+    level = models.IntegerField(default=1)
+    experience = models.IntegerField(default=0)
+    stat_points = models.IntegerField(default=0)
     
+    
+    def lvlup_exp(self):
+        return math.floor(100 * (self.level ** 1.5))
+    
+    def dodaj_exp(self, exp):
+        self.experience += exp
+        while self.experience >= self.lvlup_exp():
+
+            self.experience -= self.lvlup_exp()
+        self.save()
+        
+    def lvlup(self):
+        self.level += 1
+        self.stat_points += 5
+        self.save()
+                
     def __str__(self):
         return self.user.username
+    
