ec7259be8e6b0a80bbdb4df27a32a78290029061
Exhil95 <d.kuczka@gmail.com>
2025-03-11 17:33:48 +0100

    praca nad regeracją hp

--- klon/user_profile/models.py
+++ klon/user_profile/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import math
+import datetime as dt
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -13,6 +14,13 @@ class UserProfile(models.Model):
     stat_points = models.IntegerField(default=0)
     max_hp = models.IntegerField(default=100)
     hp = models.IntegerField(default=100)
+    base_hp = models.IntegerField(default=100)
+    defence = models.IntegerField(default=0)
+    base_defence = models.IntegerField(default=0)
+    attack = models.IntegerField(default=0)
+    base_attack = models.IntegerField(default=0)
+    gold = models.IntegerField(default=0)
+    last_regen = models.DateTimeField(default=dt.datetime.now)
     
     def lvlup_exp(self):
         return math.floor(100 * (self.level ** 1.5))
@@ -27,6 +35,15 @@ class UserProfile(models.Model):
     def lvlup(self):
         self.level += 1
         self.stat_points += 5
+        self.save()
+        
+    def hp_regen(self):
+        new_hp = self.hp + (self.constitution * 2) + 10
+        regen_timer = round((dt.now()-self.last_regen).total_seconds() / 300) 
+        if new_hp > self.max_hp:
+            self.hp = self.max_hp
+        else:
+            self.hp = new_hp
         self.save()
                 
     def __str__(self):
