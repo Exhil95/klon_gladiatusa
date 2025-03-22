41bb5111a28de26d891e1094756560a5e598e6f3
Exhil95 <d.kuczka@gmail.com>
2025-03-22 10:31:31 +0100

    dodanie redirecta ze strony głównej na login

--- klon/klon_gl/urls.py
+++ klon/klon_gl/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
     path('user_profile/', include('user_profile.urls')),
+    path('', RedirectView.as_view(url='members/login_user', permanent=False)),
 ]
