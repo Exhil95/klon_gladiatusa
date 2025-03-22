fd852286d20068f9ebf064b9924bae82dde723f7
Exhil95 <d.kuczka@gmail.com>
2025-03-22 12:15:26 +0100

    rozpoczęcie pracy nad testami

--- klon/user_profile/tests.py
+++ klon/user_profile/tests.py
@@ -1,4 +1,21 @@
 from django.test import TestCase
-import datetime as dt
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Create your tests here.
+class UserProfileModelTests(TestCase):
+    """
+    Testy modelu UserProfile.
+    """
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        UserProfile.objects.filter(user=self.user).delete()
+        self.profile = UserProfile.objects.create(user=self.user)
+
+    def test_lvlup_exp(self):
+        """
+        Test kalkulacji wymaganego expa do następnego poziomu.
+        """
+        self.profile.level = 1
+        self.assertEqual(self.profile.lvlup_exp(), 100)
+        self.profile.level = 2
+        self.assertEqual(self.profile.lvlup_exp(), 282)
