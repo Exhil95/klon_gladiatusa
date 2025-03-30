14f8f367c1d4d0fd78112ffc2ba1c5aad268ca99
Exhil95 <d.kuczka@gmail.com>
2025-03-31 01:25:13 +0200

    uruchomienie lokacji

--- klon/lokacje/urls.py
+++ klon/lokacje/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mapa, name='mapa'),
+    path('', views.mapa_view, name='mapa'),
+    path('circus/', views.circus_view, name='circus'),
+    path('beast_dungeon/', views.beast_dung_view, name='beast_dung'),
+    path('dessert_hills/', views.dessert_hills_view, name='dessert_hills'),
+    path('plains/', views.plains_view, name='plains'),
 ]
--- klon/lokacje/views.py
+++ klon/lokacje/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
-def mapa(request):
-    return render(request, 'lokacje/mapa.html', {}) 
+def mapa_view(request):
+    return render(request, 'lokacje/mapa.html', {})
+
+def beast_dung_view(request):
+    return render(request, 'lokacje/beast_dung.html', {}) 
+
+def circus_view(request):
+    return render(request, 'lokacje/circus.html', {}) 
+
+def dessert_hills_view(request):
+    return render(request, 'lokacje/dessert_hills.html', {}) 
+
+def plains_view(request):
+    return render(request, 'lokacje/plains.html', {}) 
