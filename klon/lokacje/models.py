354d8bd48ebebdc686dd7b6b9ee7e6130a63acc4
Exhil95 <d.kuczka@gmail.com>
2025-04-01 02:08:10 +0200

    rozpoczęcie modelu wroga

--- klon/lokacje/models.py
+++ klon/lokacje/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Enemy(models.Model):
+    
+    
+    name = models.TextField(max_length=25, default="test_object")
+    lvl = models.IntegerField(default=1)
+    base_strenght = models.IntegerField(default=1)
+    base_intelect = models.IntegerField(default=1)
+    base_dexterity = models.IntegerField(default=1)
+    base_constitution = models.IntegerField(default=1)
+    gold_drop = models.IntegerField(default=0)
+    base_hp = models.IntegerField(default=100)
+    base_attack = models.IntegerField(default=0)
+    base_defence = models.IntegerField(default=0)
+    #loot_table = models.ManyToManyField()
+    
+    def __str__(self):
+        return self.name
+    
+    
