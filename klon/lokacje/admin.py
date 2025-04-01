e0fa48b005903011c09a39e11b62ed5a7bc987f7
Exhil95 <d.kuczka@gmail.com>
2025-04-01 15:28:25 +0200

    update modelu

--- klon/lokacje/admin.py
+++ klon/lokacje/admin.py
@@ -6,10 +6,32 @@ from .models import Enemy
 
 @admin.register(Enemy)
 class EnemyAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "lvl", "base_strenght", "base_intelect", "base_dexterity", "base_constitution", "gold_drop", "base_hp", "base_attack", "base_defence")
-    list_filter = ("name",)
-    search_fields = ("name",)
+    # Pola wyświetlane w widoku listy obiektów
+    list_display = (
+        "id", "name", "type", "lvl", "base_strenght", "base_intelect", 
+        "base_dexterity", "base_constitution", "gold_drop", "base_hp", 
+        "base_attack", "base_defence", "created_at", "updated_at"
+    )
+    # Filtry w widoku listy obiektów
+    list_filter = ("type", "lvl", "name")
+    # Pola, po których można wyszukiwać
+    search_fields = ("name", "description")
+    # Kolejność wyświetlania obiektów
     ordering = ("-id",)
-    fields = ("name", "lvl", "base_strenght", "base_intelect", "base_dexterity", "base_constitution", "gold_drop", "base_hp", "base_attack", "base_defence")
-    list_editable = ("name", "lvl", "base_strenght", "base_intelect", "base_dexterity", "base_constitution", "gold_drop", "base_hp", "base_attack", "base_defence")
+    # Pola edytowalne w widoku listy obiektów
+    list_editable = (
+        "name", "type", "lvl", "base_strenght", "base_intelect", 
+        "base_dexterity", "base_constitution", "gold_drop", "base_hp", 
+        "base_attack", "base_defence"
+    )
+    # Pola, które można kliknąć, aby przejść do szczegółów obiektu
     list_display_links = ("id",)
+    # Pola wyświetlane w widoku szczegółów obiektu
+    fields = (
+        "name", "description", "type", "lvl", "base_strenght", 
+        "base_intelect", "base_dexterity", "base_constitution", 
+        "gold_drop", "base_hp", "base_attack", "base_defence", 
+        "loot_table", "created_at", "updated_at"
+    )
+    # Pola tylko do odczytu (np. pola automatyczne)
+    readonly_fields = ("created_at", "updated_at")
--- klon/lokacje/models.py
+++ klon/lokacje/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.urls import reverse
 
 class Enemy(models.Model):
-    
+    ENEMY_TYPES = [
+        ('normal', 'Normalny'),
+        ('elite', 'Elita'),
+        ('boss', 'Boss'),
+    ]
+
     name = models.CharField(max_length=25, default="test_object")
+    description = models.TextField(default="Opis przeciwnika", blank=True)
     lvl = models.IntegerField(default=1)
     base_strenght = models.IntegerField(default=1)
     base_intelect = models.IntegerField(default=1)
@@ -12,9 +20,19 @@ class Enemy(models.Model):
     base_hp = models.IntegerField(default=100)
     base_attack = models.IntegerField(default=0)
     base_defence = models.IntegerField(default=0)
-    #loot_table = models.ManyToManyField()
-    
+    #loot_table = models.ManyToManyField('Item', blank=True, related_name='enemies')
+    type = models.CharField(max_length=10, choices=ENEMY_TYPES, default='normal')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
-    
-    
+
+    def calculate_stats(self):
+        self.base_hp = 100 + self.lvl * 20
+        self.base_attack = self.base_strenght * 2 + self.lvl
+        self.base_defence = self.base_dexterity * 2 + self.lvl
+
+    def clean(self):
+        if self.lvl < 1:
+            raise ValidationError("Poziom przeciwnika nie może być mniejszy niż 1.")
