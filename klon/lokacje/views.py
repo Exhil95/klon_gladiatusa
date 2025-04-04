6ca3e7a81aa9cff04b4ae8b553074c86790aa53d
dutis <m.m.rduch@gmail.com>
2025-04-04 17:39:12 +0200

    dodanie lokacji i przeciwników

--- klon/lokacje/views.py
+++ klon/lokacje/views.py
@@ -4,19 +4,32 @@ from .models import Location, Enemy
 def mapa_view(request):
     location = Location.objects.get(id=1)
     location2 = Location.objects.get(id=2)
-    print(location)
-    return render(request, 'lokacje/mapa.html', {'location': location, 'location2': location2})
+    location3 = Location.objects.get(id=3)
+    location4 = Location.objects.get(id=4)
+    context = {
+        'location': location,
+        'location2': location2,
+        'location3': location3,
+        'location4': location4,
+    }
+    return render(request, 'lokacje/mapa.html', context)
 
 def beast_dung_view(request):
-    location = Location.objects.get(name="Zdziczałe Lochy")
+    location = Location.objects.get(id = 1)
     enemies = location.enemies.all()
     return render(request, 'lokacje/beast_dung.html', {'location': location, 'enemies': enemies}) 
 
 def circus_view(request):
-    return render(request, 'lokacje/circus.html', {}) 
+    location = Location.objects.get(id = 2)
+    enemies = location.enemies.all()
+    return render(request, 'lokacje/circus.html', {'location': location, 'enemies': enemies}) 
 
 def dessert_hills_view(request):
-    return render(request, 'lokacje/dessert_hills.html', {}) 
+    location = Location.objects.get(id = 3)
+    enemies = location.enemies.all()
+    return render(request, 'lokacje/dessert_hills.html', {'location': location, 'enemies': enemies}) 
 
 def plains_view(request):
-    return render(request, 'lokacje/plains.html', {}) 
+    location = Location.objects.get(id = 4)
+    enemies = location.enemies.all()
+    return render(request, 'lokacje/plains.html', {'location': location, 'enemies': enemies})
