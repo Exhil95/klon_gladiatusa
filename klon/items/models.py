93c6d43214c1aa2fae6139816ea8f4104e6e9bd5
dutis <m.m.rduch@gmail.com>
2025-05-21 01:23:39 +0200

    rozwinięcie items

--- klon/items/models.py
+++ klon/items/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 class Item(models.Model):
+    SLOT_CHOICES = [
+        ('head', 'Head'),
+        ('chest', 'Chest'),
+        ('legs', 'Legs'),
+        ('feet', 'Feet'),
+        ('hands', 'Hands'),
+        ('weapon', 'Weapons'),
+        ('shield', 'Shield'),
+        ('accessory', 'Accessory'),
+    ]
+
     name = models.CharField(max_length=100, unique=True, help_text="Nazwa przedmiotu")
     description = models.TextField(blank=True, help_text="Opis przedmiotu")
     rarity = models.CharField(
@@ -15,8 +26,16 @@ class Item(models.Model):
         default='common',
         help_text= "Rzadkość przedmiotu"
     )
+    slot = models.CharField(
+        max_length=50,
+        choices=SLOT_CHOICES,
+        help_text="Slot, do którego pasuje przedmiot"
+    )
+    item_level = models.IntegerField(help_text="Poziom przedmiotu(ilvl)")
     drop_chance = models.FloatField(help_text="Szansa na drop w procentach")
     value = models.IntegerField(default=0, help_text="Wartość przedmiotu")
 
     def __str__(self):
         return self.name
+    
+
--- klon/merchant/admin.py
+++ klon/merchant/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Merchant, Item
+from .models import Merchant, Items
 
 admin.site.register(Merchant)
-admin.site.register(Item)
