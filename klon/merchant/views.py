99784ab70ee4d3feb263c435a8b2069729f567d6
dutis <m.m.rduch@gmail.com>
2025-05-22 22:49:39 +0200

    merchant update

--- klon/merchant/views.py
+++ klon/merchant/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import Merchant
 
+import random
+
 def merchant_list(request):
-    merchants = Merchant.objects.all()
-    return render(request, 'merchant/merchant_list.html', {'merchants': merchants})
-
-
-
-
+    merchant = Merchant.objects.first()
+    items = list(merchant.items.all())
+    random_items = random.sample(items, min(len(items), 5))  # np. 5 losowych przedmiotów
+    return render(request, 'merchant/merchant_list.html', {
+        'merchant': merchant,
+        'random_items': random_items
+    })
