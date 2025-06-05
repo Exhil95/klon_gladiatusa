6c521bc25ae90da3b7bf15d4f4899f0467e5dca5
Exhil95 <d.kuczka@gmail.com>
2025-06-05 16:53:19 +0200

    inv update 3

--- klon/inventory/views.py
+++ klon/inventory/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import InventoryItem
 from items.models import Item
+from django.urls import reverse
 
 @login_required
 def backpack_view(request):
@@ -15,17 +16,17 @@ def backpack_view(request):
 @login_required
 def equip_item(request, item_id):
     item = get_object_or_404(InventoryItem, id=item_id, user=request.user)
-    InventoryItem.objects.filter(user=request.user, equipped=True, item__slot=item.item.slot).update(equipped=False)
+    InventoryItem.objects.filter(user=request.user, item__slot=item.item.slot).update(equipped=False)
     item.equipped = True
     item.save()
-    return redirect("backpack")
+    return redirect("profil")
 
 @login_required
 def unequip_item(request, item_id):
     item = get_object_or_404(InventoryItem, id=item_id, user=request.user)
     item.equipped = False
     item.save()
-    return redirect("backpack")
+    return redirect("profil")
 
 @login_required
 def give_item(request, item_id):
